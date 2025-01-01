6c43ca3e1815ec69eb59c95ac68f9cc715d35c2e
constantijn <cdemeer@fim.uni-passau.de>
2025-01-01 19:45:10 +0100

    comment moving

--- commons.h
+++ commons.h
@@ -6,15 +6,21 @@
 
 // logging
 
-#define ansi_rgb(r, g, b) ("\e[38;2;" #r ";" #g ";" #b "m") // the ansi string representation of an rgb value
-#define ansi_end ("\e[0m") // the ansi reset value
+// the ansi string representation of an rgb value
+#define ansi_rgb(r, g, b) ("\e[38;2;" #r ";" #g ";" #b "m") 
+// the ansi reset value
+#define ansi_end ("\e[0m") 
 
-#define printfn(msg, ...) (printf(msg "\n", ##__VA_ARGS__)) // printf with new-line forced
-#define log(msg, ...) (printf("%s:%d " msg "\n", __FILE__, __LINE__, ##__VA_ARGS__)) // wrapper for printf with file and line display
+// printf with new-line forced
+#define printfn(msg, ...) (printf(msg "\n", ##__VA_ARGS__)) 
+// wrapper for printf with file and line display
+#define log(msg, ...) (printf("%s:%d " msg "\n", __FILE__, __LINE__, ##__VA_ARGS__)) 
 
 #ifndef COMMONS_COLOR // define this to enable color printing
-	#define eprintfn(msg, ...) (fprintf(stderr, "[ERROR] %s:%d " msg "\n", __FILE__,  __LINE__, ##__VA_ARGS__)) // wrapper for printf for printing errors
-	#define iprintfn(msg, ...) (printf("[INFO] %s:%d " msg "\n", __FILE__, __LINE__, ##__VA_ARGS__)) // wrapper for printf for printf for debugging
+	// wrapper for printf for printing errors
+	#define eprintfn(msg, ...) (fprintf(stderr, "[ERROR] %s:%d " msg "\n", __FILE__,  __LINE__, ##__VA_ARGS__)) 
+	// wrapper for printf for printf for debugging
+	#define iprintfn(msg, ...) (printf("[INFO] %s:%d " msg "\n", __FILE__, __LINE__, ##__VA_ARGS__)) 
 #else
 	// define color printing versions of the above macros
 	#define eprintfn(msg, ...) (fprintf(stderr, "%s[ERROR] %s:%d " msg "\n%s", ansi_rgb(196, 0, 0), __FILE__, __LINE__, ##__VA_ARGS__, ansi_end))
@@ -23,9 +29,12 @@
 
 // arrays
 
-#define arr_len(arr) (sizeof((arr)) / sizeof((arr)[0])) // calculates the length of an array
+// calculates the length of an array
+#define arr_len(arr) (sizeof((arr)) / sizeof((arr)[0])) 
 
 // assertions
 
-#define cassert(state, msg, ...) do{if (!state) {eprintfn(msg, ##__VA_ARGS__); exit(1);}}while(0) // for classic assertion, uses eprintfn for printing the message
+// for classic assertion, uses eprintfn for printing the message
+#define cassert(state, msg, ...) do{if (!state) {eprintfn(msg, ##__VA_ARGS__); exit(1);}}while(0) 
+
 #endif // _COMMONS_H
